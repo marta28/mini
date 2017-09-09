e68b25085ee4ec99f7f9280d73bf9d44dd5c9e9f
marta28 <martagawor28@gmail.com>
2017-09-09 19:40:52 +0200

    map method added & java 8 examples

--- src/mini/Mini_1.java
+++ src/mini/Mini_1.java
@@ -1,7 +1,9 @@
 package mini;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Mini_1 {
 	public static void main(String[] args) {
@@ -10,20 +12,53 @@ public class Mini_1 {
 		lista.add("Danielek");
 		lista.add("nie");
 		lista.add("ma");
-		lista.add("SORENTO");
-		Integer i = count(lista);
-		System.out.println(i);
+		for (int i = 0; i < 1000000; i++) {
+			lista.add("SORENTO");
+		}
+
+		Map<String, String> mapa = new HashMap<>();
+		mapa.put("Daniel", "Daniel");
+		mapa.put("Kupa", "Kupa");
+
+		long start = System.currentTimeMillis();
+		System.out.println(count(lista));
+		System.out.println(countMap(mapa));
+		long end = System.currentTimeMillis();
+		System.out.println(end - start);
+
+		System.out.println("JAVA 8");
+		start = System.currentTimeMillis();
+		System.out.println(countJava8(lista));
+		System.out.println(countMapJava8(mapa));
+		end = System.currentTimeMillis();
+		System.out.println(end - start);
 	}
 
 	public static Integer count(List<String> lista) {
-		int result=0;
+		int result = 0;
 		for (String word : lista) {
 			int wordLenght = word.length();
-			 result += wordLenght;
+			result += wordLenght;
 		}
 		return result;
-
-
-		
 	}
+
+	public static Integer countMap(Map<String, String> mapa) {
+		int result = 0;
+		for (String key : mapa.keySet()) {
+			String word = mapa.get(key);
+			int wl = word.length();
+			result += wl;
+		}
+		return result;
+	}
+
+	public static Integer countJava8(List<String> lista) {
+		return lista.stream().mapToInt(String::length).sum();
+	}
+
+	public static Integer countMapJava8(Map<String, String> mapa) {
+		return mapa.keySet().stream().map(s -> mapa.get(s)).mapToInt(String::length).sum();
+	}
+
 }
